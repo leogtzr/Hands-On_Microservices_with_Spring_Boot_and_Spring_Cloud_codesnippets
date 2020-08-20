8d66ce2d78f4cbea23ff209d6a2e83cf54ef1dc4
leogtzr <leogutierrezramirez@gmail.com>
2020-08-20 16:55:09 -0600

    add error handling and notes on how to call the WSs

--- code/util/src/main/java/se/magnus/util/http/GlobalControllerExceptionHandler.java
+++ code/util/src/main/java/se/magnus/util/http/GlobalControllerExceptionHandler.java
@@ -4,7 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.web.bind.annotation.*;
+// import org.springframework.web.bind.annotation
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import se.magnus.util.exceptions.InvalidInputException;
 import se.magnus.util.exceptions.NotFoundException;
 
@@ -18,16 +22,13 @@ class GlobalControllerExceptionHandler {
 
     @ResponseStatus(NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public @ResponseBody
-    HttpErrorInfo handleNotFoundExceptions(ServerHttpRequest request, Exception ex) {
-
+    public @ResponseBody HttpErrorInfo handleNotFoundExceptions(ServerHttpRequest request, Exception ex) {
         return createHttpErrorInfo(NOT_FOUND, request, ex);
     }
 
     @ResponseStatus(UNPROCESSABLE_ENTITY)
     @ExceptionHandler(InvalidInputException.class)
     public @ResponseBody HttpErrorInfo handleInvalidInputException(ServerHttpRequest request, Exception ex) {
-
         return createHttpErrorInfo(UNPROCESSABLE_ENTITY, request, ex);
     }
 
