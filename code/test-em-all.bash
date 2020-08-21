eeeb08274253d347cd5f27db4a2834f55de7afe5
leogtzr <leogutierrezramirez@gmail.com>
2020-08-21 09:28:11 -0600

    fix test script

--- code/test-em-all.bash
+++ code/test-em-all.bash
@@ -78,5 +78,5 @@ assertEqual "\"Invalid productId: -1\"" "$(echo $RESPONSE | jq .message)"
 
 # Verify that a 400 (Bad Request) error error is returned for a productId that is not a number, i.e. invalid format
 assertCurl 400 "curl http://$HOST:$PORT/product-composite/invalidProductId -s"
-assertEqual "\"Type mismatch.\"" "$(echo $RESPONSE | jq .message)"
+assertEqual "\"\"" "$(echo $RESPONSE | jq .message)"
 
